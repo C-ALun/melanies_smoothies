dcabd770135f6a157aa4a2e097e9a1481e0fd3a6
Alan Chan <71807666+C-ALun@users.noreply.github.com>
2026-09-19 15:12:45 +0100

    Include SEARCH_ON column in fruit options dataframe
    Added 'SEARCH_ON' column to the dataframe and displayed it.

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,9 @@ st.write(
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col("SEARCH_ON"))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 name_on_order = st.text_input("Name on Smoothie:")
